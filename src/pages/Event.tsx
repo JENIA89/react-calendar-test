9dea4f29b0a1badc44cfe8544051ad03de6875ad
Evgene Popolamov <popolamov908@gmail.com>
2021-12-28 22:20:58 +0300

    feat: add event-form

--- src/pages/Event.tsx
+++ src/pages/Event.tsx
@@ -1,14 +1,27 @@
-import { Button, Layout, Row } from 'antd'
-import React, { FC } from 'react'
+import { Button, Layout, Modal, Row } from 'antd'
+import React, { FC, useState } from 'react'
 import EventCalendar from '../components/EventCalendar'
+import EventForm from '../components/EventForm';
 
 const Event:FC = ()=> {
+	const [isModalVisible, setIsModalVisible] = useState(false);
     return (
         <Layout>
             <EventCalendar events={[]}/>
             <Row justify='center'>
-                <Button>Добавить событие</Button>
+                <Button
+				onClick={()=>setIsModalVisible(true)}
+				>
+					Добавить событие</Button>
             </Row>
+			<Modal
+				title='Добавить событие'
+				visible={isModalVisible}
+				footer={null}
+				onCancel={()=>setIsModalVisible(false)}
+			>
+				<EventForm/>
+			</Modal>
         </Layout>
     )
 }
